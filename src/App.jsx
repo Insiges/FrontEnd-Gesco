974868bc50853b8b9bdbf539bf3398eba5129c17
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-09-03 14:37:36 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export function App() {
 	return (
 		<>
-			<div className="text-5xl font-alatsi">Gesco Gabriel</div>
+			<div className="text-5xl font-alatsi">Gesco Gabriel Teste</div>
 		</>
 	);
 }
