e205ad7e604806e78ed94999e93a9e186dd063b0
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-09-09 20:37:37 -0300

    WIP: dashboard

---
+++ src/pages/dashboard/dashboard.jsx
@@ -0,0 +1,11 @@
+export function Dashboard() {
+	return (
+		<div className="w-full grid lg:grid-cols-[905px_530px] gap-x-8 gap-y-8 ">
+			<div className="bg-transparent shadow-xl rounded-lg p-6"></div>
+			<div className="bg-transparent shadow-xl rounded-lg p-6"></div>
+			<div className="bg-transparent shadow-xl rounded-lg p-6"></div>
+			<div className="bg-transparent shadow-xl rounded-lg p-6"></div>
+			<div className="bg-transparent shadow-xl rounded-lg p-6"></div>
+		</div>
+	);
+}
