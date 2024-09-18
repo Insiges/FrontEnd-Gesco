98ef42dc91d58a5490956239d0a84afc0e7bf050
Reinan Argolo <reinanlopes.1@gmail.com>
2024-09-17 23:20:33 -0300

    feat: criação da terceira seção da landingPage

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 import FirstSection from "./components/FirstSection";
 import SecondSection from "./components/SecondSection";
+import ThirdSection from "./components/ThirdSection";
 import Header from "./components/header";
 
 export const LandingPage = () => {
 	return (
 		<>
 			<Header />
-			<FirstSection />
-			<SecondSection />
+			<Fade triggerOnce>
+				<FirstSection />
+			</Fade>
+
+			<Fade triggerOnce delay={500}>
+				<SecondSection />
+			</Fade>
+
+			<Fade triggerOnce delay={500}>
+				<ThirdSection />
+			</Fade>
 		</>
 	);
 };
