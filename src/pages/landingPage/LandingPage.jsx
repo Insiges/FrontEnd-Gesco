af26db3f80980a5839b297a1ed5aae3ef8305127
Reinan Argolo <reinanlopes.1@gmail.com>
2024-09-21 21:57:08 -0300

    feat: finalização do footer

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -3,6 +3,7 @@ import { Fade } from "react-awesome-reveal";
 import FirstSection from "./components/FirstSection";
 import SecondSection from "./components/SecondSection";
 import ThirdSection from "./components/ThirdSection";
+import Footer from "./components/footer";
 import Header from "./components/header";
 
 export const LandingPage = () => {
@@ -17,9 +18,11 @@ export const LandingPage = () => {
 				<SecondSection />
 			</Fade>
 
-			<Fade triggerOnce delay={500}>
+			<Fade triggerOnce delay={100}>
 				<ThirdSection />
 			</Fade>
+
+			<Footer />
 		</>
 	);
 };
