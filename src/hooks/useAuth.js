348673cf87907b07abe19ab6976539e1f88a8135
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-10-06 14:02:18 -0300

    WIP: hook para autenticação

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,11 +3,8 @@ import { useNavigate } from "react-router-dom";
 
 import { ACCESS_TOKEN } from "../consts/storageKeys";
 
-import {
-	getStorage,
-	removeStorage,
-	setStorage,
-} from "../service/storage/storage";
+import { API } from "../services/api/config";
+import { makeRequest } from "../services/api/requestProvider";
 
 //Hook para verificar se o usuário está autenticado além de fazer o login e logout
 export const useAuth = () => {
@@ -16,53 +13,45 @@ export const useAuth = () => {
 	const navigate = useNavigate(); // Hook para navegação.
 
 	// useEffect para verificar os tokens ao carregar a página
-	useEffect(() => {
-		const checkTokens = async () => {
-			const accessToken = getStorage(ACCESS_TOKEN); // Pega o access token do localStorage
+	// useEffect(() => {
+	// 	const checkTokens = async () => {
+	// 		const accessToken = getStorage(ACCESS_TOKEN); // Pega o access token do localStorage
 
-			if (!accessToken) {
-				return navigate("/login"); // Se não houver tokens, redireciona para a página de login
-			}
-		};
+	// 		if (!accessToken) {
+	// 			return navigate("/login"); // Se não houver tokens, redireciona para a página de login
+	// 		}
+	// 	};
 
-		checkTokens();
+	// 	checkTokens();
 
-		if (isAuthenticated) {
-			navigate("/home");
-		}
-	}, [isAuthenticated, navigate]);
+	// 	if (isAuthenticated) {
+	// 		navigate("/home");
+	// 	}
+	// }, [isAuthenticated, navigate]);
 
 	//Função para logar o usuário
 	const login = async (credentials, onError) => {
 		// Função de login da lib, onde é feita uma chamada para api para validar as credenciais fornecidas
+		console.log(credentials);
 
-		const user = users.find(
-			(user) =>
-				user.username === credentials.username &&
-				user.password === credentials.password,
-		); // Verifica o usuário e senha
-		if (!user) {
-			return onError("Credenciais inválidas"); // Retorna erro se o login falhar
-		}
-
-		// Gera o access token e o refresh token para o usuário autenticado
-		const accessToken = await generateAccessToken(user.userId);
-		const refreshToken = await generateRefreshToken(user.userId);
+		const response = await API.post("/escola/auth/login", {
+			email: credentials.username,
+			senha: credentials.password,
+		});
+		console.log({ response });
 		// Seta o estado de autenticação como true
-		setIsAuthenticated(true);
+		// setIsAuthenticated(true);
 
 		// Armazena os tokens no localStorage
-		setStorage(ACCESS_TOKEN, accessToken);
-		setStorage(REFRESH_TOKEN, refreshToken);
-
-		navigate("/home");
+		// setStorage(ACCESS_TOKEN, accessToken);
+		// setStorage(REFRESH_TOKEN, refreshToken);
 	};
 
 	//Função para deslogar o usuário
 	const logout = () => {
 		// Remove o token do localStorage
-		removeStorage(ACCESS_TOKEN);
-		removeStorage(REFRESH_TOKEN);
+		// removeStorage(ACCESS_TOKEN);
+		// removeStorage(REFRESH_TOKEN);
 		setIsAuthenticated(false);
 		navigate("/");
 	};
