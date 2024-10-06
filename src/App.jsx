82912209315b1cf1697260279336adf04916103f
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-10-05 22:11:04 -0300

    feat: passando o provider no root

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import router from "./routes/router";
 
 export function App() {
-	return <RouterProvider router={router} />
+	return (
+		<RouterProvider router={router}>
+			<AuthProvider></AuthProvider>
+		</RouterProvider>
+	);
 }
