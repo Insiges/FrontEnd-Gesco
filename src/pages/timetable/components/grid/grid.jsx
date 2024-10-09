c9f2ff4f1a7e00a4b35f83a5f2c582f7280b4713
Reinan Argolo <reinanlopes.1@gmail.com>
2024-10-09 15:38:08 -0300

    fix: ajustar arredondamento das bordas

--- src/pages/timetable/components/grid/grid.jsx
+++ src/pages/timetable/components/grid/grid.jsx
@@ -1,27 +1,29 @@
+import React, { useState } from "react";
+
 export const Grid = ({ table }) => {
 	return (
-		<div>
-			<div className="flex justify-center overflow-hidden rounded-lg border border-firstBlue">
-				<table className="table-auto">
+		<div className="flex flex-col w-full h-full">
+			<div className="flex justify-center overflow-hidden rounded-lg border border-firstBlue w-full h-full">
+				<table className="table-auto w-full h-full rounded-lg ">
 					<thead>
-						<tr>
-							<th className="px-16 py-8">Time</th>
-							<th className="px-16 py-8">Monday</th>
-							<th className="px-16 py-8">Tuesday</th>
-							<th className="px-16 py-8">Wednesday</th>
-							<th className="px-16 py-8">Thursday</th>
-							<th className="px-16 py-8">Friday</th>
+						<tr className=" bg-firstBlue">
+							<th className="px-16 py-8 text-white">Time</th>
+							<th className="px-16 py-8 text-white">Monday</th>
+							<th className="px-16 py-8 text-white">Tuesday</th>
+							<th className="px-16 py-8 text-white">Wednesday</th>
+							<th className="px-16 py-8 text-white">Thursday</th>
+							<th className="px-16 py-8 text-white">Friday</th>
 						</tr>
 					</thead>
 					<tbody>
 						{table.map((row, id) => (
 							// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-							<tr key={id}>
+							<tr key={id} className="w-full h-full">
 								{row.map((cell, id) => (
 									<td
 										// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
 										key={id}
-										className="border border-firstBlue px-16 py-8"
+										className="border border-firstBlue px-16 py-8 w-full h-full"
 									>
 										{cell}
 									</td>
--- src/pages/timetable/components/titleClass/title.jsx
+++ src/pages/timetable/components/titleClass/title.jsx
@@ -1,7 +1,7 @@
 export const TitleClass = ({ title }) => {
 	return (
 		<div>
-			<h1 className="text-2xl">{title}</h1>
+			<h1 className="text-2xl m-8">{title}</h1>
 		</div>
 	);
 };
