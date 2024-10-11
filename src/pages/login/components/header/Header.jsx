55a164fde114a04de19aab350e05a636ba612481
Isadora Allama <isadoraallama@gmail.com>
2024-10-10 23:15:31 -0300

    fix: caminho imagens

--- src/pages/login/components/header/Header.jsx
+++ src/pages/login/components/header/Header.jsx
@@ -5,7 +5,7 @@ export function Header() {
 		<nav className="bg-[#E7E7EC] p-4 flex justify-between items-center w-full h-[68px]">
 			<div className="flex items-center">
 				<img
-					src="src/pages/login/components/images/icone.png"
+					src="/src/pages/login/components/images/icone.png"
 					alt="icon"
 					className="h-10 sm:h-12 md:h-14 lg:h-16 xl:h-20 mr-2"
 				/>
