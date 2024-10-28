f425efb16e2bfc71ba42112ec2e356c99cee4b31
Felipe Santana <felipe012santana012@gmail.com>
2024-10-28 18:32:25 -0300

    Feat: component EventsTable

---
+++ src/components/EventsTable/EventsTable.jsx
@@ -0,0 +1,51 @@
+const events = [
+	{
+		id: 1,
+		name: "Workshop de React",
+		date: "2024-10-28",
+		time: "10:00 AM",
+	},
+	{
+		id: 2,
+		name: "Hackathon JavaScript",
+		date: "2024-11-01",
+		time: "09:00 AM",
+	},
+	{
+		id: 3,
+		name: "Conferência TailwindCSS",
+		date: "2024-11-15",
+		time: "02:00 PM",
+	},
+];
+
+const EventsTable = () => {
+	return (
+		<section className="p-6 max-w-7xl mx-auto">
+			<h2 className="text-3xl font-bold mb-4 text-firstBlue">
+				Eventos Cadastrados
+			</h2>
+
+			<table className="min-w-full rounded-t-3xl overflow-hidden border border-gray-300 shadow-2xl">
+				<thead className="bg-firstBlue text-white">
+					<tr className="">
+						<th className="p-3 border-b">Nome Evento</th>
+						<th className="p-3 border-b">Data</th>
+						<th className="p-3 border-b">Horário</th>
+					</tr>
+				</thead>
+				<tbody>
+					{events.map((event) => (
+						<tr key={event.id} className="text-center">
+							<td className="py-5 px-4 border-b">{event.name}</td>
+							<td className="py-5 px-4 border-b">{event.date}</td>
+							<td className="py-5 px-4 border-b">{event.time}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</section>
+	);
+};
+
+export default EventsTable;
--- src/pages/events/events.jsx
+++ src/pages/events/events.jsx
@@ -1,8 +1,11 @@
 import Calendar from "../../components/Calendar/Calendar";
+import EventsTable from "../../components/EventsTable/EventsTable";
+
 export function Events() {
 	return (
 		<div>
 			<Calendar title="Calendario de Eventos" />
+			<EventsTable />
 		</div>
 	);
 }
