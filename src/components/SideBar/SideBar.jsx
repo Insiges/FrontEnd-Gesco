6488ad02f53f8202d9d237740362f6624cb4364e
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-10-31 18:53:25 -0300

    feat: add ativiades na side bar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,6 +1,6 @@
+import { GraduationCap } from "lucide-react";
 import { FaRegCalendarAlt, FaUserTie } from "react-icons/fa";
 import { FiHome } from "react-icons/fi";
-import { GiReceiveMoney } from "react-icons/gi";
 import { GrUserManager } from "react-icons/gr";
 import { IoTime } from "react-icons/io5";
 import { MdGroups2 } from "react-icons/md";
@@ -21,21 +21,62 @@ export function SideBar() {
 				</h2>
 			</Link>
 			<ul className="flex flex-col gap-3 w-full pt-20">
-				<LinkSideBar url="/dashboard" icone={<FiHome />} nome="Dashboard" />
-				<LinkSideBar url="/students" icone={<GrUserManager />} nome="Alunos" />
-				<LinkSideBar url="/docents" icone={<FaUserTie />} nome="Docentes" />
-				<LinkSideBar
-					url="/events"
-					icone={<FaRegCalendarAlt />}
-					nome="Eventos"
-				/>
-				<LinkSideBar url="/Classes" icone={<MdGroups2 />} nome="Turmas" />
-				<LinkSideBar
-					url="/ClassesSchedule"
-					icone={<IoTime />}
-					nome="Grade de Horáros"
-				/>
+				{sideBarOptions.map((sideBarOption) => {
+					return (
+						<LinkSideBar
+							key={sideBarOption.id}
+							nome={sideBarOption.name}
+							url={sideBarOption.url}
+							icone={sideBarOption.icon}
+						/>
+					);
+				})}
 			</ul>
 		</nav>
 	);
 }
+
+const sideBarOptions = [
+	{
+		id: "1234",
+		name: "Dashboard",
+		url: "/dashboard",
+		icon: <FiHome />,
+	},
+	{
+		id: "4567",
+		name: "Alunos",
+		url: "/students",
+		icon: <GrUserManager />,
+	},
+	{
+		id: "7891",
+		name: "Docentes",
+		url: "/docents",
+		icon: <FaUserTie />,
+	},
+	{
+		id: "1011",
+		name: "Eventos",
+		url: "/events",
+		icon: <FaRegCalendarAlt />,
+	},
+	{
+		id: "1112",
+		name: "Turmas",
+		url: "/classes",
+		icon: <MdGroups2 />,
+	},
+	{
+		id: "1213",
+		name: "Grade de Horáros",
+		url: "/classesSchedule",
+		icon: <IoTime />,
+	},
+	{
+		id: "1314",
+		name: "Atividades",
+		url: "/activities",
+		icon: <GraduationCap />,
+	},
+];
