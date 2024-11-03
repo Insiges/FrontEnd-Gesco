de3d7f3c7144f34583a2697c114e0f21735abd07
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-11-03 10:27:50 -0300

    feat: route

---
+++ src/routes/router.jsx
@@ -0,0 +1,112 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Default } from "../components/layouts/default";
+import Registration from "../pages/gestaoDocente/components/MembersRegistration/MembersRegistration.jsx";
+import {
+	Activities,
+	Classes,
+	ClassesSchedule,
+	ClassesStudents,
+	ClassesStudentsAdd,
+	Dashboard,
+	Events,
+	GestaoDocente,
+	LandingPage,
+	Login,
+	Students,
+	Timetable,
+} from "../pages/index";
+import Room from "../pages/pages-teacher/components/roomBook/room";
+import RoomBook from "../pages/pages-teacher/components/roomBook/roomBook";
+
+import StudentsRegistration from "../pages/students/components/StudentRegistration/StudentsRegistration.jsx";
+import { ProtectedRoute } from "./ProtectedRoute";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <LandingPage />,
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+
+	{
+		element: <ProtectedRoute />,
+		children: [
+			{
+				element: <Default />,
+				children: [
+					{
+						path: "/dashboard",
+						element: <Dashboard />,
+					},
+					{
+						path: "/students",
+						element: <Students />,
+					},
+					{
+						path: "/students/register",
+						element: <StudentsRegistration />,
+					},
+					{
+						path: "/students/registration/edit/:id",
+						element: <StudentsRegistration />,
+					},
+					{
+						path: "/events",
+						element: <Events />,
+					},
+					{
+						path: "/docents",
+						element: <GestaoDocente />,
+					},
+					{
+						path: "/docents/register",
+						element: <Registration />,
+					},
+					{
+						path: "docents/edit/:id",
+						element: <Registration />,
+					},
+					{
+						path: "/Classes/",
+						element: <Classes />,
+					},
+					{
+						path: "/classSchedule/:id",
+						element: <Timetable />,
+					},
+					{
+						path: "/classesSchedule",
+						element: <ClassesSchedule />,
+					},
+					{
+						path: "/activities",
+						element: <Activities />,
+					},
+					{
+						path: "/Classes/:id/students",
+						element: <ClassesStudents />,
+					},
+					{
+						path: "/Classes/:id/students/add",
+						element: <ClassesStudentsAdd />,
+					},
+					{
+						path: "/sala",
+						element: <RoomBook />,
+						children: [
+							{
+								path: ":sala",
+								element: <Room />,
+							},
+						],
+					},
+				],
+			},
+		],
+	},
+]);
+
+export default router;
