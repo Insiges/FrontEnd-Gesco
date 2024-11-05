071a379e19015534e6ae6752b7863968b3be1f7b
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-11-05 16:04:48 -0300

    feat: adicionando nome da turma

--- src/pages/classesStudents/classesStudents.jsx
+++ src/pages/classesStudents/classesStudents.jsx
@@ -1,32 +1,20 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getStudentsByClass } from "../../services/api/class";
-import { useGetStudentsWithOutClass } from "../addStudentClass/hooks/useGetStudentsWithOutClass";
+
 import { Button, SearchFilter, StudentsTable } from "./components/index";
 import { useGetStudentsByClass } from "./hooks/useGetStudentsByClass";
 
 export const ClassesStudents = () => {
-	// const { data: students } = useGetStudentsWithOutClass();
 	const { id } = useParams();
 	const { data: studentsByClass } = useGetStudentsByClass(id);
 
-	// useEffect(() => {
-	// 	const fetchStudents = async () => {
-	// 		try {
-	// 			const response = await getStudentsByClass(id);
-	// 			setStudent(response);
-	// 		} catch (error) {
-	// 			console.error(error);
-	// 		}
-	// 	};
-	// 	fetchStudents();
-	// }, [id]);
+	console.log(studentsByClass);
 
 	return (
 		<div>
-			{/* // adicionar o nome da turma */}
 			<div className="flex justify-between mx-4 items-center">
-				<h1 className="text-2xl font-bold text-firstBlue">Estudantes - </h1>
+				<h1 className="text-2xl font-bold text-firstBlue">
+					Estudantes - {`${!!studentsByClass && studentsByClass[0]?.turma}`}
+				</h1>
 				<Button id={id} />
 			</div>
 			<SearchFilter />
